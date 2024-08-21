517642800c7796b6b58bcc516a2429add0c5dbc6
Eric K <ericskao@gmail.com>
2024-08-21 00:58:23 -0700

    change page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'My Dashboard | ATS',
+  title: 'Lorem Ipsum ATS',
   description: 'Manage your applications and jobs',
 };
 
